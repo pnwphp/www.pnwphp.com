ea9d37358a9560a6f72ed7ed98493ec33f6f5430
mstaples <margaret.staples@gmail.com>
2017-07-03 21:23:38 +0000

    add hotel information

--- resources/views/event/hotel.blade.php
+++ resources/views/event/hotel.blade.php
@@ -1,33 +1,31 @@
 @extends('layouts.left-sidebar')
 
 @section('main-title')
-    Coming Soon!
+    University Inn
 @endsection
 
 @section('main-content')
-    <!--
-    <p>We are hosting our speakers in the Fancy Hotel Name in downtown Seattle conveniently located a few blocks from our venue.  We have also reserved a block of rooms at a discounted price for attendees.  The number of rooms available is as generous as we could manage, but still limited.</p>
-    <p>Make your <a href="http://www.watertownseattle.com/" target="_blank">reservations</a> today!</p>
+    <p>We are hosting our speakers in the <a href="http://www.universityinnseattle.com/hotel-amenities/about-the-hotel.htm" target="_blank" rel="noopener noreferrer">University Inn</a> conveniently located a short distance from our venue.  We have also reserved a block of rooms at a discounted price for attendees.  The number of rooms available is as generous as we could manage, but still limited.</p>
+    <p>Make your <a href="http://www.universityinnseattle.com/rooms.htm" target="_blank" rel="noopener noreferrer">reservations</a> today!</p>
 
         <img src="{{ asset($image) }}" class="box feature" style="width:100%;margin:9px;">
-    -->
 @endsection
 
 @section('sidebar')
     <!-- Sidebar -->
-    <!--
     <section>
         <div style="height:20em;">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10749.69933338504!2d-122.31781506045607!3d47.656742805330694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x43198f06065ec430!2sWatertown+Hotel+-+A+Staypineapple+Hotel!5e0!3m2!1sen!2sus!4v1491537780957" frameborder="0" style="border:0; width: 100%; height: 100%" allowfullscreen></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2687.352374918228!2d-122.3196366844452!3d47.658150592408745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x549014f46f3ec3ab%3A0x9e8b16b0daa74e14!2s4140+Roosevelt+Way+NE%2C+Seattle%2C+WA+98105!5e0!3m2!1sen!2sus!4v1499115369663" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
         <br/>
+        <!--
         <h3>Booking Your Room</h3>
-        <p>When booking your room at the Fancy Hotel Name, be sure to use the promotional code PNWPHPISAWESOME to access our reserved block of rooms and benefit from the discounted price.</p>
+        <p>When booking your room at the University Inn, be sure to use the promotional code PNWPHPISAWESOME to access our reserved block of rooms and benefit from the discounted price.</p>
         <footer>
-            <a href="http://www.watertownseattle.com/" target="_blank" class="button icon fa-info-circle">Book Now</a>
+            <a href="http://www.universityinnseattle.com/rooms.htm" target="_blank" rel="noopener noreferrer" class="button icon fa-info-circle">Book Now</a>
         </footer>
+        -->
     </section>
-    -->
 @endsection
 
 
--- resources/views/event/schedule.blade.php
+++ resources/views/event/schedule.blade.php
@@ -13,9 +13,15 @@
         <a href="http://cfp.pnwphp.com/" target="_blank" rel="noopener noreferrer" class="button">Submit a Talk Now</a>
     @else
         <strong>See You There!</strong> <br/>
-        <i class="fa fa-beer"></i> We have so many amazing experiences lined up for this year's Pacific Northwest PHP conference! Starting with a special extended SeaPHP Meetup at the WeWork Holyoke Building on Wednesday evening and two tracks of workshops to choose from on <strong>Thursday at Washington University's Johnson Hall</strong>.
+
+        <i class="fa fa-beer"></i> We have so many amazing experiences lined up for this year's Pacific Northwest PHP conference! Starting with a special extended SeaPHP Meetup at the <a href="https://www.google.com/maps/place/107+Spring+St,+Seattle,+WA+98104/@47.6055108,-122.3385262,17z/data=!3m1!4b1!4m5!3m4!1s0x54906ab1bc950037:0xf006d564ded2600b!8m2!3d47.6055072!4d-122.3363375" target="_blank" rel="noopener noreferrer">WeWork Holyoke Building</a> on Wednesday evening and two tracks of workshops to choose from on <strong>Thursday at Washington University's <a href="https://www.google.com/maps/place/Johnson+Hall+(JHN)/@47.6545705,-122.3110406,17z/data=!3m1!4b1!4m5!3m4!1s0x549014f2a46431d9:0x1d835fab8ff115e!8m2!3d47.6545669!4d-122.3088519" target="_blank" rel="noopener noreferrer">Johnson Hall</a></strong>.
+
         <br/><i class="fa fa-ticket"></i>
-        The main event is two full days of fantastic talks and panels <strong>at Washington University's Kane Hall</strong> as well as the opportunity to share some meals, catch up with old friends, and make new ones!  Join us for the Pacific Northwest PHParty Friday night where we can talk about all we've learned, and celebrate the awesome developer community we share.
+        The main event is two full days of fantastic talks and panels <strong>at Washington University's <a href="https://www.google.com/maps/place/Kane+Hall+(KNE),+4069+Spokane+Ln,+Seattle,+WA+98105/@47.6549753,-122.309061,17z/data=!4m13!1m7!3m6!1s0x5490148d29600435:0x32ab53028ab205e7!2sKane+Hall+(KNE),+4069+Spokane+Ln,+Seattle,+WA+98105!3b1!8m2!3d47.6566273!4d-122.3091503!3m4!1s0x5490148d29600435:0x32ab53028ab205e7!8m2!3d47.6566273!4d-122.3091503" target="_blank" rel="noopener noreferrer">Kane Hall</a></strong> as well as the opportunity to share some meals, catch up with old friends, and make new ones!  Join us for the Pacific Northwest PHParty Friday night where we can talk about all we've learned, and celebrate the awesome developer community we share.
+
+        <br/><i class="fa fa-smile-o"></i>
+        Join us <strong>Friday Night</strong> from 7pm until 9:30pm at <a href="https://www.google.com/maps/place/Watertown+Hotel+-+A+Staypineapple+Hotel/@47.6595469,-122.319617,17z/data=!3m1!4b1!4m5!3m4!1s0x549014f45c8d7335:0x43198f06065ec430!8m2!3d47.6595433!4d-122.3174284" target="_blank" rel="noopener noreferrer">The Watertown Hotel</a> for the Pacific Northwest PHP Conference Party.
+
         <br/><i class="fa fa-cutlery"></i>
         If you're still in town on <strong>Sunday</strong> (and we hope you will be!) we will be meeting up from 11am until 2pm at <a href="http://www.shultzys.com/" target="_blank" rel="noopener noreferrer">Shultzy's Bar and Grill</a> for a PHP Community tradition: WurstCon.
     @endif
